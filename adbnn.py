36ad4478efe4f6961c5bea91293f422e6baca8af
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-03-17 19:27:14 +0530

    update

--- adbnn.py
+++ adbnn.py
@@ -208,6 +208,7 @@ class DatasetProcessor:
 
         self.org_data=self.data.copy()
         self.data=self.data.drop(columns not in [self.data_config['column_names']])
+        print(self.data)
         X = self.data.drop(columns=[self.data_config['target_column']])
         y = self.data[self.data_config['target_column']]
 
