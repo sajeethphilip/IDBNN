0ebf3fd1dccee49915d5f909daa4a342bf2240fb
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-03-17 19:28:09 +0530

    update

--- adbnn.py
+++ adbnn.py
@@ -211,7 +211,7 @@ class DatasetProcessor:
         print(self.data)
         X = self.data.drop(columns=[self.data_config['target_column']])
         y = self.data[self.data_config['target_column']]
-
+        input("Press Key")
         # Initialize training log
         log_file = os.path.join(output_dir, f'{dataset_name}_log.csv')
         self.training_log = pd.DataFrame(columns=[
