c7da35307ad7b409d2ce78f61bb154bc3cbaca0b
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-03-24 10:56:12 +0530

    update

--- cdbnn2.py
+++ cdbnn2.py
@@ -6595,7 +6595,7 @@ def main():
         args = parse_arguments()
 
         # Process based on mode
-        ls if args.mode == 'predict':
+        if args.mode == 'predict':
             # Load the config
             config_path = os.path.join(args.output_dir, args.data, f"{args.data}.json")
             with open(config_path, 'r') as f:
