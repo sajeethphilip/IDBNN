c4e38d00b931be1dd9295a266f7636792c22c619
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-03-24 08:58:39 +0530

    update

--- cdbnn2.py
+++ cdbnn2.py
@@ -193,7 +193,7 @@ class PredictionManager:
         # Set default output CSV path if not provided
         if output_csv is None:
             dataset_name = self.config['dataset']['name']
-            output_csv = os.path.join('data', dataset_name, f"{dataset_name}.csv")
+            output_csv = os.path.join('data', dataset_name, f"{dataset_name}_predictions.csv")
 
         # Get the image transform from the config
         transform = self._get_transforms()
@@ -213,7 +213,7 @@ class PredictionManager:
             csv_writer = csv.writer(csvfile)
             # Write header
             feature_cols = [f'feature_{i}' for i in range(self.config['model']['feature_dims'])]
-            csv_writer.writerow(['filename', 'true_class'] + [f'phase1_{col}' for col in feature_cols] + [f'{col}' for col in feature_cols])
+            csv_writer.writerow(['filename', 'true_class'] + [f'phase1_{col}' for col in feature_cols] + [f'phase2_{col}' for col in feature_cols])
 
         # Process images in batches
         for i in tqdm(range(0, len(image_files), batch_size), desc="Predicting features"):
