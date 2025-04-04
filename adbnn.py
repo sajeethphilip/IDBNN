89befc7e4b8a4a475c47fb3d97e6a576c8522710
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-04-04 19:32:56 +0530

    update

--- adbnn.py
+++ adbnn.py
@@ -5903,7 +5903,7 @@ def plot_confusion_matrix(confusion_mat: np.ndarray, class_names: np.ndarray, da
 
 
 def load_label_encoder(dataset_name):
-    encoder_path = f'Model/Best_Histogram _{dataset_name}_label_encoder.pkl'
+    encoder_path = f'Model/Best_Histogram_{dataset_name}_label_encoder.pkl'
     if os.path.exists(encoder_path):
         with open(encoder_path, 'rb') as f:
             label_encoder = pickle.load(f)
