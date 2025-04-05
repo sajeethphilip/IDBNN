1a07ac0dc3b5e8bd68284f1fcee766be5c0d3db7
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-04-05 16:11:37 +0530

    update

--- adbnn.py
+++ adbnn.py
@@ -5230,7 +5230,7 @@ class DBNN(GPUDBNN):
             results = self._generate_detailed_predictions(
                 X_orig=X,  # Pass the original features
                 predictions=y_pred,
-                true_labels=y_true_str if y_true_str is not None else None,
+                true_labels=(y_true_str if y_true_str is not None else None),
                 posteriors
             )
 
