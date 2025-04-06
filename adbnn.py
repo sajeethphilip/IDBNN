c8681739be326cef0da9e93d1bf2249626a01630
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-04-06 09:38:26 +0530

    update

--- adbnn.py
+++ adbnn.py
@@ -3266,10 +3266,10 @@ class DBNN(GPUDBNN):
                     # Add page break if not the last page
                     if page_num < n_pages - 1:
                         elements.append(PageBreak())
-                    pbar.update(1)
-                    # Build the PDF ONCE after all elements are ready
-                    doc.build(elements)
-                #print(f"\033[K✅ {class_name} - Saved {n_images} images to {pdf_path}")
+                pbar.update(1)
+            # Build the PDF ONCE after all elements are ready
+            doc.build(elements)
+            #print(f"\033[K✅ {class_name} - Saved {n_images} images to {pdf_path}")
 #--------------Option 3 ----------------
     def generate_class_pdf(self, image_paths: List[str], posteriors: np.ndarray, output_pdf: str):
         """Generate professional multi-page PDF with 2x4 image grids per class, sorted by confidence.
