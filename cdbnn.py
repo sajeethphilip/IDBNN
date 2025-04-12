8a02e187cbb284a3d4a91a350cb236f507a04452
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-04-13 01:57:35 +0530

    update

--- cdbnn.py
+++ cdbnn.py
@@ -2578,7 +2578,7 @@ class ModelFactory:
             if model.in_channels != config['dataset']['in_channels']:
                 logger.warning(f"Model expects {model.in_channels} channels but config specifies {config['dataset']['in_channels']}")
 
-        return model.to(device)
+        return model
 
 
 # Update the training loop to handle the new feature dictionary format
