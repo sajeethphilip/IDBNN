a407146329ab1f71b49288306b59a6b55a4401df
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-04-14 23:48:36 +0530

    update

--- cdbnn.py
+++ cdbnn.py
@@ -2883,7 +2883,7 @@ def _train_phase(model: nn.Module, train_loader: DataLoader,
                 )
             else:
                 patience_counter += 1
-            print(f"{Colors.GREEN}Patience counter: {patience_counter} with avg loss:{avg_loss} and Best loss at {best_loss}{Colors.ENDC}",end="\r",flush=True)
+            print("\033[K" +f"{Colors.GREEN}Patience counter: {patience_counter} with avg loss:{avg_loss} and Best loss at {best_loss}{Colors.ENDC}",end="\r",flush=True)
             if patience_counter >= config['training'].get('early_stopping', {}).get('patience', 5):
                 logger.info(f"Early stopping triggered for phase {phase} after {epoch + 1} epochs")
                 break
