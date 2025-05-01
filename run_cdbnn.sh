7c5d2cdba1b2a0d950edd2df74008b7a0c260f5c
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-05-02 01:35:00 +0530

    Update run_cdbnn.sh

--- run_cdbnn.sh
+++ run_cdbnn.sh
@@ -11,6 +11,9 @@ read -p "Enter mode (train/predict/all/fresh): " mode
 # Prompt for encoder type (sutoenc/cnn)
 read -p "Enter encoder type for cdbnn (autoencoder/cnn): " encoder_type
 
+# Prompt for model type (Histogram/Gaussian)
+read -p "Enter model type for adbnn (Histogram/Gaussian): " model
+
 
 # Git pull
 git pull
@@ -27,12 +30,12 @@ case $mode in
     echo "Running training..."
     python cdbnn.py --mode train --data_name "$data_name" --input_path "$input_path" --encoder_type "$encoder_type"
     python cdbnn.py --mode predict --data_name "$data_name" --input_path "$input_path" --encoder_type "$encoder_type"
-    python adbnn.py --file_path "data/${data_name}/${data_name}.csv" --mode train
+    python adbnn.py --file_path "data/${data_name}/${data_name}.csv" --mode train --model_type "$model"
     ;;&  # Continue to next case (executes predict if mode is "all")
   "predict" | "all" | "fresh")
     echo "Running prediction..."
     python cdbnn.py --mode predict --data_name "$data_name" --input_path "$input_path" --encoder_type "$encoder_type"
-    python adbnn.py --file_path "data/${data_name}/${data_name}.csv" --mode predict
+    python adbnn.py --file_path "data/${data_name}/${data_name}.csv" --mode predict --model_type "$model"
     ;;
   *)
     echo "Invalid mode. Choose 'train', 'predict', or 'all'"
