ecc365b1e1ea9fb3894ca93e7a7324fffb149061
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-05-03 06:41:56 +0530

    Create run_cdbnn0.sh

---
+++ run_cdbnn0.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Prompt for data name
+read -p "Enter data name (e.g., galaxies): " data_name
+
+read -p "Enter data path (e.g., Data/galaxies/): " input_path
+
+# Prompt for mode (train/predict/all)
+read -p "Enter mode (train/predict/all/fresh): " mode
+
+# Prompt for encoder type (sutoenc/cnn)
+read -p "Enter encoder type for cdbnn (autoenc/cnn): " encoder_type
+
+# Prompt for model type (Histogram/Gaussian)
+read -p "Enter model type for adbnn (Histogram/Gaussian): " model
+
+
+# Git pull
+git pull
+
+# Run operations based on mode
+case $mode in
+  "fresh")
+    echo "Cleaning..."
+    rm -rf "data/${data_name}/"
+    rm -rf Model/*"${data_name}"_*
+    rm -rf training_data/${data_name}/
+    ;;&
+  "train" | "all"| "fresh")
+
+    echo "Running training..."
+    python cdbnn0.py --mode train --data "$input_path" --encoder_type "$encoder_type"
+    python cdbnn0.py --mode predict --data "$input_path" --encoder_type "$encoder_type"
+    python adbnn.py --file_path "data/${data_name}/${data_name}.csv" --mode train --model_type "$model"
+    ;;&  # Continue to next case (executes predict if mode is "all")
+  "predict" | "all" | "fresh")
+    echo "Running prediction..."
+    python cdbnn0.py --mode predict --data "$input_path" --encoder_type "$encoder_type"
+    python adbnn.py --file_path "data/${data_name}/${data_name}.csv" --mode predict --model_type "$model"
+    ;;
+  *)
+    echo "Invalid mode. Choose 'train', 'predict', or 'all'"
+    exit 1
+    ;;
+esac
+
+echo "Pipeline completed!"
