b0aac0520344ba415a20a329f8e2af8afdca88b5
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-05-06 09:11:19 +0530

    update

--- adbnn.py
+++ adbnn.py
@@ -370,9 +370,9 @@ class DatasetConfig:
         config["active_learning"]= {
             "tolerance": 1.0,
             "cardinality_threshold_percentile": 95,
-            "strong_margin_threshold": 0.1,
-            "marginal_margin_threshold": 0.1,
-            "min_divergence": 0.3
+            "strong_margin_threshold": 0.3,
+            "marginal_margin_threshold": 0.3,
+            "min_divergence": 0.5
         }
         config["execution_flags"]= {
             "train": True,
@@ -2397,9 +2397,9 @@ class DBNN(GPUDBNN):
     def _select_samples_from_failed_classes(self, test_predictions, y_test, test_indices):
         """Cluster-based selection with robust dimension handling."""
         active_learning_config = self.config.get('active_learning', {})
-        strong_margin_threshold = active_learning_config.get('strong_margin_threshold', 0.1)
-        marginal_margin_threshold = active_learning_config.get('marginal_margin_threshold', 0.1)
-        min_divergence = active_learning_config.get('min_divergence', 0.3)
+        strong_margin_threshold = active_learning_config.get('strong_margin_threshold', 0.3)
+        marginal_margin_threshold = active_learning_config.get('marginal_margin_threshold', 0.3)
+        min_divergence = active_learning_config.get('min_divergence', 0.5)
         max_class_addition_percent = active_learning_config.get('max_class_addition_percent', 5)
 
         test_predictions = torch.as_tensor(test_predictions, device=self.device)
@@ -6470,9 +6470,9 @@ def load_or_create_config(config_path: str) -> dict:
         "active_learning": {
             "tolerance": 1.0,
             "cardinality_threshold_percentile": 95,
-            "strong_margin_threshold": 0.1,
-            "marginal_margin_threshold": 0.1,
-            "min_divergence": 0.3
+            "strong_margin_threshold": 0.3,
+            "marginal_margin_threshold": 0.3,
+            "min_divergence": 0.5
         },
         "execution_flags": {
             "train": True,
